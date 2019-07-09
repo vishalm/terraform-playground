1431e5bf68aa406475ffeb0a48ce9c57a7950389
Vishal <vishalm@users.noreply.github.com>
2019-07-09 20:07:43 +0400

    Resources and connections tf (#3)

---
+++ connections.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+
+provider "azurerm" {
+   subscription_id = "0"
+   client_id = "1"
+   client_secret = "2"
+}
---
+++ resources.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "enviorment-example-two" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    tags = {
+        Name = "terraform-aws-vpc-example-two"
+    }
+}
+
+resource "aws_subnet" "subnet1" {
+    cidr_block = "${cidrsubnet(aws_vpc.enviorment-example-two.cidr_block,3,1)}"
+    vpc_id = "${aws_vpc.enviorment-example-two.id}"
+    availability_zone = "us-west-2a"
+}
