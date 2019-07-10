32441a24962dc8794e1aaef4aca8137f5f9c5263
s799985@dnata.com <s799985@emirates.com>
2019-07-10 13:54:56 +0400

    Added security subnet

--- resources.tf
+++ resources.tf
@@ -12,3 +12,21 @@ resource "aws_subnet" "subnet1" {
     vpc_id = "${aws_vpc.enviorment-example-two.id}"
     availability_zone = "us-west-2a"
 }
+
+
+resource "aws_subnet" "subnet2" {
+    cidr_block = "${cidrsubnet(aws_vpc.enviorment-example-two.cidr_block,2,2)}"
+    vpc_id = "${aws_vpc.enviorment-example-two.id}"
+    availability_zone = "us-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+    vpc_id = "${aws_vpc.enviorment-example-two.id}"
+    # Configure a new service to be monitored by an Icinga2 Server
+   ingress {
+       cidr_blocks = ["${aws_vpc.enviorment-example-two.cidr_block}"]
+       from_port = 80
+       to_port = 80
+       protocol = "tcp"
+   }
+}
